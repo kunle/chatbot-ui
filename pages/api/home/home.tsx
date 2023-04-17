800b88d655682bfa63735e470b9c4c7000fa4daa
Kunle Adeniyi <kunle4@gmail.com>
2023-04-16 23:15:55 -0400

    Made to host on good prompt

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -355,13 +355,16 @@ const Home = ({
       }}
     >
       <Head>
-        <title>Chatbot UI</title>
-        <meta name="description" content="ChatGPT but better." />
+        <title>Good prompts based on Chatbot UI</title>
+        <meta name="description" content="Good prompts for AI" />
         <meta
           name="viewport"
           content="height=device-height ,width=device-width, initial-scale=1, user-scalable=no"
         />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
       </Head>
       {selectedConversation && (
         <main
